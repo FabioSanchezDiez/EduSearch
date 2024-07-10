7a80d69ef0a296d46b1c213e795b2e536e9e18c5
FabioSanchezDiez <fabiodev3@gmail.com>
2024-07-10 20:52:20 +0200

    API | feat: add method to create and update users

--- api/src/Repository/UserRepository.php
+++ api/src/Repository/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
@@ -14,9 +15,20 @@ use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
  */
 class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, private EntityManagerInterface $entityManager)
     {
         parent::__construct($registry, User::class);
+    }
+
+    /**
+     * @param User $user
+     * @return bool
+     */
+    public function createOrUpdateUser(User $user): bool
+    {
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        return true;
     }
 
     /**
