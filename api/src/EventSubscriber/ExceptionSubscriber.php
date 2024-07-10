0c3372d7bf6a0d2c731ae6291aa64fb4dabe1f23
FabioSanchezDiez <fabiodev3@gmail.com>
2024-07-10 19:45:08 +0200

    API | feat: add ExceptionSubscriber to handle all application exceptions

---
+++ api/src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $statusCode = $exception->getStatusCode();
+        } else {
+            $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+        }
+        $message = $exception->getMessage();
+
+        $event->setResponse(new JsonResponse(['error' => $message], $statusCode));
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
