163f68d39c137a9073b91a4fa1efa219634338dd
FabioSanchezDiez <fabiodev3@gmail.com>
2024-07-11 18:58:36 +0200

    API | config: add fixtures dependency

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
