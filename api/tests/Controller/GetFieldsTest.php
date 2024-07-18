397da2de644a7c79732383e591481c94f27d3a84
FabioSanchezDiez <fabiodev3@gmail.com>
2024-07-18 14:37:33 +0200

    API | refactor: refactor get all fields test

--- api/tests/Controller/GetFieldsTest.php
+++ api/tests/Controller/GetFieldsTest.php
@@ -62,21 +62,22 @@ class GetFieldsTest extends WebTestCase
         $this->assertNotNull($content);
         $this->assertCount(3, $fields);
 
-        //Sort by name
-        usort($fields, function($a, $b) {
-            return strcmp($a['name'], $b['name']);
-        });
+        $field = $this->fieldRepository->find('0edbed88-f546-4bee-b16a-035c16abc5b4');
+        $this->assertNotNull($field);
+        $this->assertEquals('Informática y Comunicaciones', $field->getName());
+        $this->assertEquals('Este campo incluye el desarrollo de hardware y software, redes de comunicación, ciberseguridad, inteligencia artificial y análisis de datos, facilitando la conectividad global y la innovación tecnológica en diversos sectores.', $field->getDescription());
+        $this->assertEquals('https://firebasestorage.googleapis.com/v0/b/edusearch-1c3c8.appspot.com/o/Inform%C3%A1tica_-_EduSearch-NO-BG.png?alt=media&token=58c4d331-1500-4df8-8d68-996be82a447e', $field->getImage());
 
-        $this->assertEquals('Informática y Comunicaciones', $fields[0]['name']);
-        $this->assertEquals('Este campo incluye el desarrollo de hardware y software, redes de comunicación, ciberseguridad, inteligencia artificial y análisis de datos, facilitando la conectividad global y la innovación tecnológica en diversos sectores.', $fields[0]['description']);
-        $this->assertEquals('https://firebasestorage.googleapis.com/v0/b/edusearch-1c3c8.appspot.com/o/Inform%C3%A1tica_-_EduSearch-NO-BG.png?alt=media&token=58c4d331-1500-4df8-8d68-996be82a447e', $fields[0]['image']);
+        $field2 = $this->fieldRepository->find('14051bb8-3a6b-4882-a8fe-70c3c633b3ca');
+        $this->assertNotNull($field2);
+        $this->assertEquals('Sanidad', $field2->getName());
+        $this->assertEquals('Este campo incluye la atención médica, enfermería, salud pública, investigación biomédica, gestión sanitaria, farmacia, tecnología médica, rehabilitación y fisioterapia, promoción de la salud, y la prevención de enfermedades, garantizando el bienestar, el tratamiento efectivo y la mejora continua de la salud de la población.', $field2->getDescription());
+        $this->assertEquals('https://firebasestorage.googleapis.com/v0/b/edusearch-1c3c8.appspot.com/o/Salud_-_EduSearch-NO-BG.png?alt=media&token=e65f3cd0-361f-41ab-806d-c887d26c92c1', $field2->getImage());
 
-        $this->assertEquals('Sanidad', $fields[1]['name']);
-        $this->assertEquals('Este campo incluye la atención médica, enfermería, salud pública, investigación biomédica, gestión sanitaria, farmacia, tecnología médica, rehabilitación y fisioterapia, promoción de la salud, y la prevención de enfermedades, garantizando el bienestar, el tratamiento efectivo y la mejora continua de la salud de la población.', $fields[1]['description']);
-        $this->assertEquals('https://firebasestorage.googleapis.com/v0/b/edusearch-1c3c8.appspot.com/o/Salud_-_EduSearch-NO-BG.png?alt=media&token=e65f3cd0-361f-41ab-806d-c887d26c92c1', $fields[1]['image']);
-
-        $this->assertEquals('Servicios Socioculturales y a la Comunidad', $fields[2]['name']);
-        $this->assertEquals('Este campo incluye la intervención social, gestión cultural, animación sociocultural, trabajo comunitario, atención a la diversidad, educación social, servicios de bienestar, promoción de la igualdad y la inclusión, y el desarrollo de programas comunitarios, fortaleciendo la cohesión social y mejorando la calidad de vida de las comunidades.', $fields[2]['description']);
-        $this->assertEquals('https://firebasestorage.googleapis.com/v0/b/edusearch-1c3c8.appspot.com/o/Servicios_Socioculturales_-_EduSearch-NO-BG.png?alt=media&token=0deeadc1-cc93-4c68-b1ed-a50a0b35b05f', $fields[2]['image']);
+        $field3 = $this->fieldRepository->find('94443462-8e93-46e0-8025-e00b3fbda869');
+        $this->assertNotNull($field3);
+        $this->assertEquals('Servicios Socioculturales y a la Comunidad', $field3->getName());
+        $this->assertEquals('Este campo incluye la intervención social, gestión cultural, animación sociocultural, trabajo comunitario, atención a la diversidad, educación social, servicios de bienestar, promoción de la igualdad y la inclusión, y el desarrollo de programas comunitarios, fortaleciendo la cohesión social y mejorando la calidad de vida de las comunidades.', $field3->getDescription());
+        $this->assertEquals('https://firebasestorage.googleapis.com/v0/b/edusearch-1c3c8.appspot.com/o/Servicios_Socioculturales_-_EduSearch-NO-BG.png?alt=media&token=0deeadc1-cc93-4c68-b1ed-a50a0b35b05f', $field3->getImage());
     }
 }
