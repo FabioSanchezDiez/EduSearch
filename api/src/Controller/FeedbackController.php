04d269a7f97ad4797e9134d0b88dc6a6efef1b61
FabioSanchezDiez <fabiodev3@gmail.com>
2024-07-18 22:54:47 +0200

    API | feat: create endpoint to return feedback by program id

---
+++ api/src/Controller/FeedbackController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\FeedbackRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+#[Route('/api', name: 'api_')]
+class FeedbackController extends AbstractController
+{
+    public function __construct(private FeedbackRepository $feedbackRepository, private SerializerInterface $serializer){}
+
+    #[Route('/feedback/program/{programId}', name: 'feedback_by_program', methods: ['GET'])]
+    public function programsByField(string $programId): Response
+    {
+        $programs = $this->feedbackRepository->findBy(['program' => $programId]);
+        $data = $this->serializer->serialize($programs, 'json');
+        return new Response($data, Response::HTTP_OK, ['Content-Type' => 'application/json']);
+    }
+}
