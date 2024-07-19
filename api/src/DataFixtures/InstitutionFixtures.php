336333031dc58d0156b16300cb68b4c804d091d6
FabioSanchezDiez <fabiodev3@gmail.com>
2024-07-19 13:28:31 +0200

    API | chore: change strategy in Institution entity and add ids to institution fixtures

--- api/src/DataFixtures/InstitutionFixtures.php
+++ api/src/DataFixtures/InstitutionFixtures.php
@@ -19,6 +19,7 @@ class InstitutionFixtures extends Fixture implements DependentFixtureInterface
 
         foreach ($this->institutionsData as $institutionInfo) {
             $institution = new Institution();
+            $institution->setId(Uuid::fromString($institutionInfo['id']));
             $institution->setName($institutionInfo['name']);
             $institution->setDescription($institutionInfo['description']);
             $institution->setLastUpdate(new \DateTime($institutionInfo['last_update']));
@@ -36,18 +37,21 @@ class InstitutionFixtures extends Fixture implements DependentFixtureInterface
     {
         $this->institutionsData = [
             [
+                'id' => '5f78bcf8-ddba-4717-bb44-6d71420d1608',
                 'name' => 'IES Hermenegildo Lanz',
                 'description' => 'Centro público',
                 'last_update' => '2024-07-17',
                 'programs' => ['2c0892fe-8a29-426b-8c18-e8fb92bf5868', '3eee4e2a-5072-48b3-a9c3-8f0c36a76fc0'],
             ],
             [
+                'id' => '6c1faa18-ffef-42ee-8ad5-2e9f58781a24',
                 'name' => 'IES Francisco Ayala',
                 'description' => 'Centro público',
                 'last_update' => '2024-07-17',
                 'programs' => ['2c0892fe-8a29-426b-8c18-e8fb92bf5868', '3eee4e2a-5072-48b3-a9c3-8f0c36a76fc0'],
             ],
             [
+                'id' => '77f8f054-dba7-4f49-8c9b-74960a59915a',
                 'name' => 'IES Zaidín Vergeles',
                 'description' => 'Centro público',
                 'last_update' => '2024-07-17',
--- api/src/Entity/Institution.php
+++ api/src/Entity/Institution.php
@@ -15,7 +15,7 @@ class Institution
 {
     #[ORM\Id]
     #[ORM\Column(type: "uuid", unique: true)]
-    #[ORM\GeneratedValue(strategy: "CUSTOM")]
+    #[ORM\GeneratedValue(strategy: "NONE")]
     #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
     protected UuidInterface $id;
 
@@ -49,6 +49,11 @@ class Institution
     public function getId(): UuidInterface
     {
         return $this->id;
+    }
+
+    public function setId(UuidInterface $id): void
+    {
+        $this->id = $id;
     }
 
     public function getName(): ?string
