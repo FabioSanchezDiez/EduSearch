cd828ac2f90c907b44fb26439a07158a37714f13
FabioSanchezDiez <fabiodev3@gmail.com>
2024-07-22 14:41:29 +0200

    API | feat: create confirm user endpoint

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class UserController extends AbstractController
 {
     public function __construct(private UserService $userService){}
-    
+
     #[Route('/users/register', name: 'users_create', methods: ['POST'])]
     public function registerUser(Request $request): JsonResponse
     {
@@ -21,5 +21,14 @@ class UserController extends AbstractController
         $this->userService->registerUser($userData);
 
         return new JsonResponse(["success" => "Usuario creado correctamente"], Response::HTTP_CREATED);
+    }
+
+    #[Route('/users/confirm/{token}', name: 'users_confirm', methods: ['PUT'])]
+    public function confirmUser(string $token): JsonResponse
+    {
+        $success = $this->userService->confirmUser($token);
+        if(!$success) return new JsonResponse(["error" => "Usuario no existente o token no valido"], Response::HTTP_NOT_FOUND);
+
+        return new JsonResponse(["success" => "Usuario confirmado correctamente"], Response::HTTP_OK);
     }
 }
