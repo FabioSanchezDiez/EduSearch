2e8ac171c349c389e8211fe62e8a7c9b34494d85
FabioSanchezDiez <fabiodev3@gmail.com>
2024-07-23 15:00:50 +0200

    API | feat: add jwt event subscriber to manage login responses

---
+++ api/src/EventSubscriber/JWTEventSubscriber.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class JWTEventSubscriber implements EventSubscriberInterface
+{
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event): void
+    {
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        if (!$user->isVerified()) {
+            $event->setData(["error" => "Usuario no confirmado"]);
+            return;
+        }
+
+        $data = array_merge(
+            ["name" => $user->getName(), "email" => $user->getEmail()],
+            $event->getData()
+        );
+
+        $event->setData($data);
+    }
+
+    public function onAuthenticationFailure(AuthenticationFailureEvent $event): void
+    {
+        $code = Response::HTTP_UNAUTHORIZED;
+        $response = new JsonResponse([
+            "error" => "Las credenciales son incorrectas",
+            "code" => $code
+        ], $code);
+
+        $event->setResponse($response);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'lexik_jwt_authentication.on_authentication_success' => 'onAuthenticationSuccess',
+            'lexik_jwt_authentication.on_authentication_failure' => 'onAuthenticationFailure',
+        ];
+    }
+}
