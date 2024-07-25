bd0b99cecb4a0aab1ff81eccc04ecfbad16b1c75
FabioSanchezDiez <fabiodev3@gmail.com>
2024-07-25 21:54:20 +0200

    Frontend | chore: change app image and background color in navbar and footer

--- frontend/src/components/ui/navigation/navbar.tsx
+++ frontend/src/components/ui/navigation/navbar.tsx
@@ -7,7 +7,7 @@ import Navlinks from "./navlinks";
 export default function Navbar() {
   return (
     <header>
-      <nav className="flex items-center justify-center flex-col md:flex-row md:gap-8 lg:gap-12 gap-4 p-6 shadow-xl bg-background">
+      <nav className="flex items-center justify-center flex-col md:flex-row md:gap-8 lg:gap-12 gap-4 p-6 shadow-xl dark:shadow-none bg-background dark:bg-zinc-900">
         <Link href={"/"}>
           <Image
             src="/img/EduSearchLogo.png"
--- frontend/src/components/ui/site-footer.tsx
+++ frontend/src/components/ui/site-footer.tsx
@@ -1,7 +1,7 @@
 export function SiteFooter() {
   return (
-    <footer className="bg-zinc-800 dark:bg-background p-8 sticky top-[100vh] mt-16">
-      <p className="text-white text-center text-xl font-bold">
+    <footer className="bg-zinc-800 dark:bg-zinc-900 p-8 sticky top-[100vh] mt-16">
+      <p className="text-white text-center font-medium">
         Desarrollado por Fabio Sánchez Diez &#169;
       </p>
     </footer>
