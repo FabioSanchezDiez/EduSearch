cacdd598dc426752d1d6d11bc92be5bf8b586327
FabioSanchezDiez <fabiodev3@gmail.com>
2024-07-29 14:08:43 +0200

    Frontend | feat: create middleware to protect dashboard routes

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { useSession } from "next-auth/react";
+
+export default function Dashboard() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+  console.log(session);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <pre>
+        <code>{JSON.stringify(session, null, 2)}</code>
+      </pre>
+    </div>
+  );
+}
