7bd23389c77fac658fa0a7c2c546ed044b46286d
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-01 15:46:50 +0200

    Frontend | feat: create generic skeleton

--- frontend/src/app/fields/page.tsx
+++ frontend/src/app/fields/page.tsx
@@ -1,11 +1,12 @@
 import Fields from "@/components/ui/programs/fields";
+import GenericSkeleton from "@/components/ui/skeletons/generic-skeleton";
 import { Suspense } from "react";
 
 export default function FieldsPage() {
   return (
     <div className="flex flex-col gap-6">
       <h1 className="text-2xl font-semibold">Disciplinas Académicas</h1>
-      <Suspense fallback={<div>Cargando...</div>}>
+      <Suspense fallback={<GenericSkeleton></GenericSkeleton>}>
         <Fields></Fields>
       </Suspense>
     </div>
