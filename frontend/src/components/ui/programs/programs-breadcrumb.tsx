465d796a5d791ab58ea94a6e24538f4faa09c0d0
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-02 15:37:34 +0200

    Frontend | feat: create breadcrumb component

---
+++ frontend/src/components/ui/programs/programs-breadcrumb.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { unformatString } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function ProgramsBreadcrumb() {
+  const paths = usePathname();
+  const pathNames = paths.split("/").filter((path) => path);
+
+  console.log(pathNames);
+  return (
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          {pathNames.length === 1 ? (
+            <BreadcrumbItem>
+              <BreadcrumbPage>Familias Profesionales</BreadcrumbPage>
+            </BreadcrumbItem>
+          ) : pathNames.length === 2 ? (
+            <>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href="/fields">Familias Profesionales</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>{unformatString(pathNames[1])}</BreadcrumbPage>
+              </BreadcrumbItem>
+            </>
+          ) : (
+            <>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href="/fields">Familias Profesionales</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild>
+                  <Link href={`/fields/${pathNames[1]}`}>
+                    {unformatString(pathNames[1])}
+                  </Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>{unformatString(pathNames[2])}</BreadcrumbPage>
+              </BreadcrumbItem>
+            </>
+          )}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </>
+  );
+}
