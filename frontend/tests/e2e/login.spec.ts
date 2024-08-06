79e665c29ccc78bc62d1fe20a9db8635bcab0736
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-06 18:20:20 +0200

    Frontend | tests: add steps to login test

--- frontend/tests/e2e/login.spec.ts
+++ frontend/tests/e2e/login.spec.ts
@@ -1,22 +1,28 @@
 import { test, expect } from "@playwright/test";
 
 test("Login page test", async ({ page }) => {
-  await page.goto("http://localhost:3000/accounts/login");
+  await test.step("Load page", async () => {
+    await page.goto("http://localhost:3000/accounts/login");
+  });
 
-  const emailInput = page.locator('input[type="email"]');
-  await expect(emailInput).toHaveCount(1);
+  await test.step("Find and fill inputs and submit them", async () => {
+    const emailInput = page.locator('input[type="email"]');
+    await expect(emailInput).toHaveCount(1);
 
-  const passwordInput = page.locator('input[type="password"]');
-  await expect(passwordInput).toHaveCount(1);
+    const passwordInput = page.locator('input[type="password"]');
+    await expect(passwordInput).toHaveCount(1);
 
-  const submitButton = page.locator('button[type="submit"]');
-  await expect(submitButton).toHaveCount(1);
+    const submitButton = page.locator('button[type="submit"]');
+    await expect(submitButton).toHaveCount(1);
 
-  await emailInput.fill("test@example.com");
-  await passwordInput.fill("password");
+    await emailInput.fill("test@example.com");
+    await passwordInput.fill("password");
 
-  await submitButton.click();
+    await submitButton.click();
+  });
 
-  const alertDiv = page.locator('div[role="alert"]');
-  await expect(alertDiv).toHaveCount(1);
+  await test.step("Find feedback alert", async () => {
+    const alertDiv = page.locator('div[role="alert"]');
+    await expect(alertDiv).toHaveCount(1);
+  });
 });
