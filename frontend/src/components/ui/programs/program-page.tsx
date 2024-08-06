9a16b679abba8eab65aeb4cadbafa01ae4f571f2
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-06 18:11:06 +0200

    Frontend | feat: add alternative message to null properties

--- frontend/src/components/ui/programs/program-page.tsx
+++ frontend/src/components/ui/programs/program-page.tsx
@@ -9,25 +9,36 @@ export default async function ProgramPage({
 }) {
   const program: Program = await fetchProgramByName(programName);
 
+  const renderField = (
+    field: any,
+    defaultMessage: string = "No se han encontrado datos"
+  ) => {
+    return field || defaultMessage;
+  };
+
   return (
     <>
-      <h1 className="text-2xl font-semibold">{program?.name}</h1>
+      <h1 className="text-2xl font-semibold">{renderField(program?.name)}</h1>
       <section className="flex flex-col gap-4">
         <div>
           <h2 className="text-xl">Descripción</h2>
-          <p>{program?.description}</p>
+          <p>{renderField(program?.description)}</p>
         </div>
         <div>
           <h2 className="text-xl">Requisitos Académicos</h2>
-          <p>{program?.priorEducation}</p>
+          <p>{renderField(program?.priorEducation)}</p>
         </div>
         <div>
           <h2 className="text-xl">Información Adicional</h2>
-          <a href={program?.additionalInformation} target="_blank">
-            <Button variant={"secondary"} className="mt-1">
-              Consultar
-            </Button>
-          </a>
+          {program?.additionalInformation ? (
+            <a href={program.additionalInformation} target="_blank">
+              <Button variant={"secondary"} className="mt-1">
+                Consultar
+              </Button>
+            </a>
+          ) : (
+            <p>No se han encontrado datos</p>
+          )}
         </div>
       </section>
     </>
