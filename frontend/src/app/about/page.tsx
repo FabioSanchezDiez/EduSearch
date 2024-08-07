bcb627820cb10001b5878c452f87b1acab9fc728
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-07 18:46:56 +0200

    Frontend | feat: create about page

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import About from "@/components/ui/about/about";
+
+export default function AboutPage() {
+  return (
+    <main className="my-16 flex flex-col gap-12">
+      <About></About>
+    </main>
+  );
+}
