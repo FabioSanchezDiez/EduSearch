d582bf699ef1e617a926403219607d7a01e34a4a
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-08 15:43:43 +0200

    API | config: set CORS configuration

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
