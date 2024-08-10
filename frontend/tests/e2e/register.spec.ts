28080c94d6bf97e0f1d45947625dc758c070e718
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-10 20:00:10 +0200

    Frontend | tests: create register e2e test

---
+++ frontend/tests/e2e/register.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+import { AuthPage } from "./pages/AuthPage";
+
+test("Register page test", async ({ page }) => {
+  const registerPage = new AuthPage(page);
+
+  await test.step("Load page", async () => {
+    await registerPage.goto("/accounts/register");
+  });
+
+  await test.step("Find and fill inputs and submit them", async () => {
+    await expect(registerPage.usernameInput).toHaveCount(1);
+    await expect(registerPage.emailInput).toHaveCount(1);
+    await expect(registerPage.passwordInput).toHaveCount(1);
+    await expect(registerPage.provinceInput).toHaveCount(1);
+    await expect(registerPage.submitButton).toHaveCount(1);
+
+    await registerPage.register(
+      "TestUser",
+      "test@example.com",
+      "password",
+      "Granada"
+    );
+  });
+
+  await test.step("Find feedback alert", async () => {
+    await expect(registerPage.alertDiv).toHaveCount(1);
+  });
+});
