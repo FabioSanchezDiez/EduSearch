ce0b5333a62a299397907b4886c45e00ac6d8841
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-18 17:34:15 +0200

    Frontend | tests: add programs route constant instead of magic string

--- frontend/tests/e2e/programs.spec.ts
+++ frontend/tests/e2e/programs.spec.ts
@@ -1,11 +1,12 @@
 import { test } from "@playwright/test";
 import { ProgramsPage } from "./pages/ProgramsPage";
+import { PROGRAMS_PAGE_ROUTE } from "@/lib/routes";
 
 test("Programs page test", async ({ page }) => {
   const programsPage = new ProgramsPage(page);
 
   await test.step("Load page", async () => {
-    await programsPage.goto("/programs");
+    await programsPage.goto(PROGRAMS_PAGE_ROUTE);
   });
 
   await test.step("Find computer science field and click it", async () => {
