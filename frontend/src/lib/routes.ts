8ddffe2a6029778b8436100fd935c335ec8a0b72
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-18 17:58:21 +0200

    Frontend | feat: create register page route constant

--- frontend/src/lib/routes.ts
+++ frontend/src/lib/routes.ts
@@ -2,4 +2,5 @@ export const HOME_PAGE_ROUTE = "/";
 export const ABOUT_PAGE_ROUTE = "/sobre-nosotros";
 export const PROGRAMS_PAGE_ROUTE = "/programas";
 export const DASHBOARD_PAGE_ROUTE = "/area-personal";
-export const LOGIN_PAGE_ROUTE = "/cuentas/login";
+export const LOGIN_PAGE_ROUTE = "/cuentas/entrar";
+export const REGISTER_PAGE_ROUTE = "/cuentas/registro";
