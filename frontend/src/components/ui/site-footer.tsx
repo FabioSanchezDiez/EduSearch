251c51e16ecf73c2dc42685772e459fbb16a1d20
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-21 20:10:32 +0200

    Frontend | chore: change navbar and footer bg color in light mode

--- frontend/src/components/ui/site-footer.tsx
+++ frontend/src/components/ui/site-footer.tsx
@@ -1,7 +1,7 @@
 export function SiteFooter() {
   return (
-    <footer className="bg-zinc-800 dark:bg-zinc-900 p-8 sticky top-[100vh] mt-16">
-      <p className="text-white text-center font-medium">
+    <footer className="bg-gray-200 dark:bg-zinc-900 p-8 sticky top-[100vh] mt-16">
+      <p className="text-center font-medium">
         Desarrollado por Fabio Sánchez Diez &#169;
       </p>
     </footer>
