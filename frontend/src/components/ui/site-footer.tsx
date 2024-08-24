373ca6b2e31e36cc4b5817727ede98e68000eafd
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-24 18:57:23 +0200

    Frontend | feat: enhance site footer

--- frontend/src/components/ui/site-footer.tsx
+++ frontend/src/components/ui/site-footer.tsx
@@ -1,9 +1,18 @@
+import { Button } from "./button";
+
 export function SiteFooter() {
   return (
-    <footer className="bg-gray-200 dark:bg-zinc-900 p-8 sticky top-[100vh] mt-16">
-      <p className="text-center font-medium">
-        Desarrollado por Fabio Sánchez Diez &#169;
-      </p>
+    <footer className="bg-gray-200 dark:bg-zinc-900 p-5 sticky top-[100vh] mt-16 flex flex-col justify-center items-center">
+      <p className="text-center font-medium">EduSearch 2024-2025 &#169;</p>
+      <div className="flex justify-center items-center">
+        <a
+          href="https://github.com/FabioSanchezDiez/EduSearch"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button variant={"navigation"}>Enlace al repositorio</Button>
+        </a>
+      </div>
     </footer>
   );
 }
