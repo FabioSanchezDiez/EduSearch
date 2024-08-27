792e29bb36905f765e6af21a493d508aeb7fc697
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-27 16:51:03 +0200

    API | config: create MAIL_CONFIRMATION_URL env

--- api/src/Service/EmailService.php
+++ api/src/Service/EmailService.php
@@ -73,7 +73,7 @@ class EmailService
             "    <h1>EduSearch</h1>\n" .
             "    <h2>Gracias por registrarte ". $name . "!</h2>\n" .
             "    <p>Por favor confirma tu correo electronico para que puedas comenzar a utilizar tu cuenta en EduSearch</p>\n" .
-            "    <a href='"."http://localhost:3000/cuentas/confirmar/". $token ."'><button>Confirmar cuenta</button></a>\n" .
+            "    <a href='".$_ENV['MAIL_CONFIRMATION_URL']."/cuentas/confirmar/". $token ."'><button>Confirmar cuenta</button></a>\n" .
             "    <p>Si tu no te registraste, por favor ignora este correo electrónico.</p>\n" .
             "    <div><p></p></div>\n" .
             "    <p><span>Este correo electronico fue enviado desde una dirección solamente de notificaciones que no puede aceptar correo electronico entrante. Por favor no respondas a este mensaje.</span></p>\n" .
