251eae9fb3e71d51acb3b12c7965747bc1f1c809
FabioSanchezDiez <fabiodev3@gmail.com>
2024-08-30 14:13:29 +0200

    Frontend | refactor: change some details in site footer

--- frontend/src/components/ui/site-footer.tsx
+++ frontend/src/components/ui/site-footer.tsx
@@ -1,18 +1,19 @@
+import { Github } from "lucide-react";
 import { Button } from "./button";
 
 export function SiteFooter() {
   return (
     <footer className="bg-gray-200 dark:bg-zinc-900 p-5 sticky top-[100vh] mt-16 flex flex-col justify-center items-center">
-      <p className="text-center font-medium">EduSearch 2024-2025 &#169;</p>
-      <div className="flex justify-center items-center">
-        <a
-          href="https://github.com/FabioSanchezDiez/EduSearch"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Button variant={"navigation"}>Enlace al repositorio</Button>
-        </a>
-      </div>
+      <p className="text-center font-medium">EduSearch 2024 &#169;</p>
+      <a
+        href="https://github.com/FabioSanchezDiez/EduSearch"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button variant={"navigation"} className="flex gap-1 items-center">
+          Enlace al repositorio <Github className="w-4 h-4"></Github>
+        </Button>
+      </a>
     </footer>
   );
 }
